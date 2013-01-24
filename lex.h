8c7bf1c141e3bba2c2469bf7a4706f4cf6c23040
Anil Kag <anilkagak2@gmail.com>
2013-01-24 17:06:51 +0530

    - comments in lex.h header in order to match the rest of the code

--- lex.h
+++ lex.h
@@ -1,25 +1,26 @@
-#define EOI		    0	/* End of input			*/
-#define SEMI	    1	/* ; 				*/
+#define EOI		0	/* End of input			*/
+#define SEMI	    	1	/* ; 				*/
 #define PLUS 		2	/* + 				*/
 #define TIMES		3	/* * 				*/
-#define LP		    4	/* (				*/
-#define RP		    5	/* )				*/
-#define NUM     	6	/* Decimal Number or Identifier */
-#define MINUS       7
-#define DIV         8
-#define EQUAL       9
-#define LT          10
-#define GT          11
+#define LP		4	/* (				*/
+#define RP		5	/* )				*/
+#define NUM     	6	/* Decimal Number		*/
+#define MINUS      	7	/* -				*/
+#define DIV        	8	/* /				*/
+#define EQUAL       	9	/* ==	here it is =		*/
+#define LT          	10	/* <				*/
+#define GT          	11	/* >				*/
 
-#define BEGIN       12
-#define END         13
-#define IF          14
-#define THEN        15
-#define WHILE       16
-#define DO          17
-#define ASSIGN      18
+#define BEGIN       	12	/* begin			*/
+#define END         	13	/* end				*/
+#define IF          	14	/* if				*/
+#define THEN        	15	/* then				*/
+#define WHILE       	16	/* while			*/
+#define DO          	17	/* do				*/
+#define ASSIGN      	18	/* assign			*/
 
-#define ID          19
+#define ID          	19	/* Identifier			 */
+
 extern char *yytext;		/* in lex.c			*/
 extern int yyleng;
 extern yylineno;
