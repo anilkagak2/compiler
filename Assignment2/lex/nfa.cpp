bc0aa81fd3caed00f714895a1df543e1696e4033
Harsh Mehta <harsh.version.1@gmail.com>
2013-03-02 15:07:23 +0530

    Concat implemented

--- Assignment2/lex/nfa.cpp
+++ Assignment2/lex/nfa.cpp
@@ -17,7 +17,7 @@ nfa::nfa () {
     transitions[0][EPSILON].insert (1);
 }
 
-nfa
+void
 nfa::union_nfa (nfa &n) {
 	int states_add = n.transitions.size ();
 
@@ -43,16 +43,13 @@ nfa::union_nfa (nfa &n) {
 	num_states += n.num_states + 2;
 	transitions.push_back (start_new);
 	transitions.push_back (final_new);
-	return this;
 }
 
-nfa
+void
 nfa::kleene_star_nfa () {
    
     //get new rows
-    vector< set<int> > start,final;
-    start.resize(MAX_ALPHABET);
-    final.resize(MAX_ALPHABET);
+    vector< set<int> > start(MAX_ALPHABET),final(MAX_ALPHABET);
 
     //some definitions for clarity
     int old_start = num_states-2;
@@ -74,13 +71,60 @@ nfa::kleene_star_nfa () {
     num_states += 2;
 }
 
-nfa
+void
 nfa::concate_nfa (nfa &n) {
     
-    for(int i=0; i< n.transitions.size(); i++){
-        
+    int states_add = n.transitions.size ();
+
+	for (int i=0; i<states_add; i++) {
+		for (int j=0; j<n.transitions[i].size (); j++) {
+			set<int>::iterator it;
+			for (it=n.transitions[i][j].begin (); it != n.transitions[i][j].end (); it++) {
+				*it += num_states;	// increment the state index with this->new_states
+			}
+		}
+		transitions.push_back (n.transitions[i]);
+	}
+
+    /*
+	vector< set<int> > start_new (MAX_ALPHABET);
+	vector< set<int> > final_new (MAX_ALPHABET);
+	start_new[EPSILON].insert (num_states-2);			// start state of this
+	start_new[EPSILON].insert (num_states + n.num_states-2);	// start state of n
+    */
     
-    }
+    //some definitions for clarity
+    int old_start_1 = num_states-2;
+    int old_final_1 = num_states-1;
+    int old_start_2 = num_states + n.num_states - 2;
+    int old_final_2 = num_states + n.num_states - 1;
+    
+	transitions[old_final_1][EPSILON].insert (old_start_2);	// old_final_1 -> old_start_2 
+
+    /*
+	transitions.push_back (start_new);
+	transitions.push_back (final_new);
+	*/
+
+    vector< set<int> > start;
+	start_row = transitions[old_start_1];
+    transitions.erase(old_start_1);
+    transitions.insert(transitions.begin()+transitions.size()-2,start_row);
+    
+	for (int i=0; i<transitions.size(); i++) {
+		for (int j=0; j<transitions[i].size (); j++) {
+			set<int>::iterator it;
+			for (it=n.transitions[i][j].begin (); it != n.transitions[i][j].end (); it++) {
+                
+                if(*it >= (num_states-1) && *it <= (num_states + n.num_states - 2)){
+                    *it -= 1;	// decrement the state index with 1 if its index was changed during erase and insert process.
+			    }
+            }
+		}
+	}
+    
+    alphabet.insert (n.alphabet.begin (), n.alphabet.end ());
+	num_states += n.num_states;
 
 }
 
